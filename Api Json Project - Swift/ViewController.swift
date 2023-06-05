a77632e59e304688b2672ad552e309997c61b47b
cozdemir <cengiz.ozdemir@assistt.com.tr>
2023-06-05 16:26:48 +0300

    jsonResponse

--- Api Json Project - Swift/ViewController.swift
+++ Api Json Project - Swift/ViewController.swift
@@ -44,12 +44,32 @@ class ViewController: UIViewController {
               //  2. adım
                 if data != nil {
                     do {
-                    let jsonResponse = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers)
+                    let jsonResponse = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers) as! Dictionary<String, Any>
                         //ASYNC
                         DispatchQueue.main.async {
-                            print(jsonResponse)
+                            if let rates = jsonResponse["rates"] as? [String : Any] {
+                               // print(rates)
+                                if let cad = rates["CAD"] as? Double {
+                                    self.cadLabel.text = "CAD : \(cad)"
+                            }
+                                if let chf = rates["CHF"] as? Double {
+                                    self.chfLabel.text = "CHF : \(chf)"
+                            }
+                                if let gbp = rates["GBP"] as? Double {
+                                    self.gdpLabel.text = "GBP : \(gbp)"
+                            }
+                                if let jpy = rates["JPY"] as? Double {
+                                    self.jpyLabel.text = "JPY : \(jpy)"
+                            }
+                                if let usd = rates["USD"] as? Double {
+                                    self.usdLabel.text = "UST : \(usd)"
+                            }
+                                if let turksih = rates["TRY"] as? Double {
+                                    self.tryLabel.text = "TRY : \(turksih)"
+                            }
                         }
                         
+                    }
                     } catch {
                         print("error")
                     }
