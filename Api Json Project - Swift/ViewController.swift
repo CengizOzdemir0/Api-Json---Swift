68646fc3de09b67e1ba20d44c291fa9a81187eab
cozdemir <cengiz.ozdemir@assistt.com.tr>
2023-06-05 12:17:26 +0300

    Initial Commit

---
+++ Api Json Project - Swift/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Api Json Project - Swift
+//
+//  Created by sys on 5.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var cadLabel: UILabel!
+    
+    @IBOutlet weak var chfLabel: UILabel!
+    
+    @IBOutlet weak var gdpLabel: UILabel!
+    
+    @IBOutlet weak var jpyLabel: UILabel!
+    @IBOutlet weak var usdLabel: UILabel!
+    @IBOutlet weak var tryLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func getRatesClicked(_ sender: Any) {
+        // url oluşturmak için 1.adım .
+    }
+}
+
